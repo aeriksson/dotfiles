cb3ab811c1045b7905c73cba28eb391921e52e74
Andre Eriksson <andre.eriksson@trioptima.com>
2014-06-18 13:35:56 +0200

    Add custom zsh theme.

--- bootstrap.sh
+++ bootstrap.sh
@@ -73,6 +73,7 @@ setup_vim() {
 setup_zsh() {
     command_exists zsh || install_command zsh
     link_file "${PROGDIR}/zsh/oh-my-zsh" "${HOME}/.oh-my-zsh"
+    link_file "${PROGDIR}/zsh/themes" "${PROGDIR}/zsh/oh-my-zsh/custom/themes"
     link_file "${PROGDIR}/zsh/zshrc" "${HOME}/.zshrc"
     link_file "${PROGDIR}/profile" "${HOME}/.profile"
     link_file "${PROGDIR}/aliases" "${HOME}/.aliases"
